b30a3b5122c8f827479adaa00ce93dd7ab68d18b
xietianduo <>
2024-05-23 11:37:53 +0800

    add jitpack

---
+++ buildSrc/src/main/kotlin/com/goldenduo/buildsrc/ProjectSetting.kt
@@ -0,0 +1,10 @@
+package com.goldenduo.buildsrc
+
+class ProjectSetting {
+    companion object{
+        const val groupId="com.goldenduo"
+        const val artifactId="catrpc"
+        const val name="Cat RPC"
+        const val description="An RPC framework for embedded system"
+    }
+}
--- rpc/build.gradle.kts
+++ rpc/build.gradle.kts
@@ -1,7 +1,11 @@
+import com.goldenduo.buildsrc.ProjectSetting
+
 plugins {
     kotlin("jvm") version Versions.KOTLIN
     id("com.github.gmazzo.buildconfig") version Versions.BUILDCONFIG
+    `maven-publish`
 }
+
 buildConfig {
     packageName="com.goldenduo.catrpc"
     buildConfigField("VERSION", 1)
@@ -10,6 +14,33 @@ buildConfig {
 }
 tasks.test {
     useJUnitPlatform()
+}
+
+val sourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.getByName("main").allSource)
+    from("LICENCE.md") {
+        into("META-INF")
+    }
+}
+configure<PublishingExtension> {
+    publications {
+        register("release", MavenPublication::class) {
+            groupId = ProjectSetting.groupId
+            artifactId = ProjectSetting.artifactId
+            version = version
+            from(components["java"])
+            artifact(sourcesJar)
+            pom {
+                packaging = "jar"
+                name.set(ProjectSetting.name)
+                description.set(ProjectSetting.description)
+            }
+        }
+    }
+    repositories {
+        maven { url = uri("https://jitpack.io") }
+    }
 }
 dependencies {
     api("io.netty:netty-all:4.1.109.Final")
@@ -21,7 +52,6 @@ dependencies {
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.KOTLIN_COROUTINES}")
     implementation("com.github.sarveswaran-m:util.concurrent.blockingMap:0.91")
     implementation("commons-codec:commons-codec:1.15")
-
     testImplementation(kotlin("test"))
 }
 
